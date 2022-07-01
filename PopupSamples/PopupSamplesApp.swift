2a8962566853e32e41a9dc295ced5a59d099b52f
Wataru Fujiwara <wataru930@gmail.com>
2022-07-01 22:15:03 +0900

    Initial Commit

---
+++ PopupSamples/PopupSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopupSamplesApp.swift
+//  PopupSamples
+//
+//  Created by Wataru Fujiwara on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct PopupSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
